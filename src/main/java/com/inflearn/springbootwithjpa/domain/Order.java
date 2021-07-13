46c282fe0a569a6d67c565ebf08fc2eb496e2c80
SEOLMIN KWON <stat_kwon@naver.com>
2021-07-13 20:36:49 +0900

    [연관관계] N:M관계 (1:M,M:1 처리 방법 중 1:M 부분)  Order -> OrderItem -> Item
    ( 설명 )
    - 주문(Order)와 상품(Item)의 관계는 M:M 관계이다. 하지만 쿼리가 N+1번 나오게되는 N+1문제 때문에 실제 구축할 때 1:M, M:1관계로 풀어서 해결해야 됨
    - 이에 따라 주문(Order)와 상품(Item) 사이에 주문상품이라는 FK를 2개 가지고 있는 Table을 구축하여 N:M관계를 해결함
    - 현재 부분은 주문(Order)와 주문상품(OrderItem)인 1:M 관계 구축 부분에 해당함
    
    (추가)
    enum 타입인 주문상태(OrderStatus)를 추가 하였음.

--- src/main/java/com/inflearn/springbootwithjpa/domain/Order.java
+++ src/main/java/com/inflearn/springbootwithjpa/domain/Order.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="orders") // order 내장함수와 겹치기 때문에 관례적으로 orders라고 붙임
@@ -17,4 +20,12 @@ public class Order {
     @ManyToOne // order와 member는 M:1 관계이다.
     @JoinColumn(name="member_id") // FK의 table에서 이름이 member_id가 된다.
     private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    // private Date date; -> Java8이전에 쓰던 방식 지금은 아래와 같이 사용
+    private LocalDateTime orderDate;
+
+    private OrderStatus status; // Enum 타입으로 주문상태를 나타냄 (ORDER, CANCEL)
 }
