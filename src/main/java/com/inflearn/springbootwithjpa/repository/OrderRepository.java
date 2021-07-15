9c068018904130bf69d2e0d5cca8782a92fe0da2
SEOLMIN KWON <stat_kwon@naver.com>
2021-07-15 19:20:16 +0900

    [Order]  Search기능 추가
    동적쿼리 하는 방법은 더 공부해야 함

--- src/main/java/com/inflearn/springbootwithjpa/repository/OrderRepository.java
+++ src/main/java/com/inflearn/springbootwithjpa/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,8 +21,12 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // 동적 쿼리는 추후 설명 예정정
-//   public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+   public List<Order> findAll(OrderSearch orderSearch) {
+       return em.createQuery("select o from Order o join  o.member m" +
+               " where o.status= :status and m.name like :name", Order.class) // 아래부분이 이것이 동적으로 돌아가야 함
+               .setParameter("status", orderSearch.getOrderStatus())
+               .setParameter("name", orderSearch.getMemberName())
+               .setMaxResults(1000) // 페이징 때 쓸 수 있음 최대 1000건
+               .getResultList();
+   }
 }
