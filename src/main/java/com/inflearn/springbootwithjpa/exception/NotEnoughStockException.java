75c7e2a47add13a54944b21639d32bcbc6c3d9f7
SEOLMIN KWON <stat_kwon@naver.com>
2021-07-15 11:02:43 +0900

    [Item] Item Entity 비지니스 로직 추가 + 예외

---
+++ src/main/java/com/inflearn/springbootwithjpa/exception/NotEnoughStockException.java
@@ -0,0 +1,20 @@
+package com.inflearn.springbootwithjpa.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+}
+
